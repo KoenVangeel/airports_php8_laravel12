92645a829ab911dd2fcca35d6a7b3edb5f6c2c57
Koen Vangeel <u0063152@thomasmore.be>
2026-02-24 09:04:36 +0100

    kleine aanpassing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <div class="min-h-screen flex flex-col items-center justify-start pt-50 gap-4">
         <p class="text-center text-xl font-light text-gray-700 max-w-xl">
             Welcome to Airports,
-            a sample application written with Laravel/Livewire to search and book flights.
+            a sample application written with PHP 8 / Laravel 12 to search and book flights.
         </p>
 
         <flux:icon name="plane-takeoff" class="w-30 h-30 text-accent" />
